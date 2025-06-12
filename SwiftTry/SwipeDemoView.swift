e77ba36bcb83c3ef42373441599d95c0d4f1cbe2
Taufik <taufikalislam@gmail.com>
2025-06-12 04:21:37 -0400

    Picker,Tabview and UI

---
+++ SwiftTry/SwipeDemoView.swift
@@ -0,0 +1,18 @@
+//
+//  SwipeDemoView.swift
+//  SwiftTry
+//
+//  Created by Anika Tabasum on 6/17/25.
+//
+
+import SwiftUI
+
+struct SwipeDemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwipeDemoView()
+}
