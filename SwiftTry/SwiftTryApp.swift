13cbe6275b272898a1c11e9aa156b3de8570485e
Taufik <taufikalislam@gmail.com>
2025-06-12 04:06:41 -0400

    Initial Commit

---
+++ SwiftTry/SwiftTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTryApp.swift
+//  SwiftTry
+//
+//  Created by Anika Tabasum on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
