3e25fb34d9b0faf70d016db889116396c184cf54
Taufik <taufikalislam@gmail.com>
2025-06-25 01:32:36 -0400

    Data Persistence @AppStorage and UserDefaults

---
+++ SwiftTry/StorageUI.swift
@@ -0,0 +1,18 @@
+//
+//  StorageUI.swift
+//  SwiftTry
+//
+//  Created by Anika Tabasum on 6/24/25.
+//
+
+import SwiftUI
+
+struct StorageUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StorageUI()
+}
