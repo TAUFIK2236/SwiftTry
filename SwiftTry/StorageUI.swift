c6aa767f6b95230f166354c11e7ad09ab10f04c1
Taufik <taufikalislam@gmail.com>
2025-06-25 01:32:36 -0400

    Data Persistence

---
+++ SwiftTry/StorageUI.swift
@@ -0,0 +1,33 @@
+//
+//  StorageUI.swift
+//  SwiftTry
+//
+//  Created by Anika Tabasum on 6/24/25.
+//
+
+import SwiftUI
+
+struct StorageUI: View {
+    @AppStorage("MyAnimal") var favoriteAnimal:String = "Cat"
+    @State private var name: String = ""
+    
+    var body: some View {
+        VStack(spacing:20){
+            Text("My favorite animal name is : \(favoriteAnimal)")
+            Text(favoriteAnimal).font(.largeTitle)
+            TextField("Enter the favorite animalname",text: $favoriteAnimal)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Text("Type your name and save it")
+            TextField("Enter name",text:$name).textFieldStyle(RoundedBorderTextFieldStyle())
+            Button("Save Name"){
+                UserDefaults.standard.set(name, forKey:"MyName")
+            }
+           
+        }
+    }
+}
+
+#Preview {
+    StorageUI()
+}
