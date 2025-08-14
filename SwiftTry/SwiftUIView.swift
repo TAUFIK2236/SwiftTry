c18898fbb29c93308be7d087c811cb61be253239
Taufik <taufikalislam@gmail.com>
2025-08-14 16:02:44 -0400

    Learing Porpose

---
+++ SwiftTry/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  SwiftTry
+//
+//  Created by Anika Tabasum on 8/14/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
